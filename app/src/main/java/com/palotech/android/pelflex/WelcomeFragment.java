22cb3747ea51a5e8864d113fb9c0285c20438206
Kristo Palo <kristopalo@hotmail.com>
2018-05-30 01:29:19 +0300

    first snow

---
+++ app/src/main/java/com/palotech/android/pelflex/WelcomeFragment.java
@@ -0,0 +1,34 @@
+package com.palotech.android.pelflex;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Date;
+
+public class WelcomeFragment extends Fragment {
+
+    private TextView mTitleTextView;
+    private TextView mTextTextView;
+    private Date mLastWorkoutDate;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_welcome, container, false);
+
+        mTitleTextView = v.findViewById(R.id.welcome_title);
+        mTextTextView = v.findViewById(R.id.welcome_text);
+
+        return v;
+    }
+}
